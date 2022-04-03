3280c8f421eb636d9801db683da8476d3bbc2e8d
jonah.jtr <tillmanjonah@gmail.com>
2022-04-02 23:44:48 -0500

    uploaded radio inputs

--- src/Forms/Form.js
+++ src/Forms/Form.js
@@ -4,6 +4,7 @@ function Form() {
   const [values, setvalues] = useState({
     firstname: "",
     lastname: "",
+    radioOption: "",
   }); // [values] = variable for inputs   // [setvalues] = changes variable contents
 
   function handleChange(e) {
@@ -11,15 +12,18 @@ function Form() {
     const value = e.target.value;
     setvalues((values) => ({ ...values, [name]: value })); // (...) followed by variable is called spreading. (pretends as if you wrote each value)
     //[name] = for each input you put a name, and this lets it update specific inputs. value = whatever is input
+    console.log(values);
   }
 
   function handleSubmit(e) {
     e.preventDefault(); //prevents page refreshing when submitting form
-    console.log(values); // put whatever function needed to submit(usually api call) delete console log after it works!
+    console.log({ values }); // put whatever function needed to submit(usually api call) delete console log after it works!
   }
   return (
     <div>
       <form onSubmit={handleSubmit}>
+        {/*----------------------- start of text inputs-------------------------------------*/}
+
         <div className="form-textinputs">
           <label> First Name: </label>
           <input
@@ -36,6 +40,31 @@ function Form() {
             value={values.lastname}
           ></input>
         </div>
+        {/*----------------------- end of text inputs------------------------------------*/}
+
+        {/*----------------------- start of radio inputs---------------------------------*/}
+        <div className="form-radioinput">
+          <h1>Do you understand forms? {values.radioOption} </h1>
+          <input
+            type="radio"
+            name="radioOption"
+            value="yes"
+            checked={values.radioOption === "yes"}
+            onChange={handleChange}
+          ></input>
+          <label>yes</label>
+          <input
+            type="radio"
+            name="radioOption"
+            value="no"
+            checked={values.radioOption === "no"}
+            onChange={handleChange}
+          ></input>
+          <label>no</label>
+        </div>
+        {/*----------------------- end of radio inputs-------------------------------------*/}
+        {/*----------------------- start of checkbox inputs--------------------------------*/}
+        {/*----------------------- end of checkbox inputs----------------------------------*/}
 
         <input type="submit" value="submit"></input>
       </form>
